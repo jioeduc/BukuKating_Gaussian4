d4a461111fd9ca4a9ce7f7332699a2c9cee77a20
LidiaNatasyah <lidia.123450015@student.itera.ac.id>
2024-11-03 16:36:53 +0700

    lidia syudahhh

--- Buku Kating/015_Lidia Natasyah Marpaung.py
+++ Buku Kating/015_Lidia Natasyah Marpaung.py
@@ -1461,9 +1461,9 @@ elif menu == "Departemen SSD":
                 "nama": "Farrel Julio Akbar", #6
                 "nim": "122450110",
                 "umur": "20",
-                "asal":"Bogor",
-                "alamat": "Jl.lapas",
-                "hobbi": "Supporteran",
+                "asal":"Salatiga",
+                "alamat": "Bogor",
+                "hobbi": "Jl.lapas",
                 "sosmed": "@farrel__julio",
                 "kesan":"abangnya asik, gokil parah!!",  
                 "pesan":"semangat kuliahnya bang!!"
